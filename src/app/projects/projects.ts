e0780aac435f00f4f28afa4598571dbce9f1387b
Dmitry Minchenok <dminchenok@diasoft.ru>
2026-01-18 17:48:06 +0300

    added filters block

--- src/app/projects/projects.ts
+++ src/app/projects/projects.ts
@@ -1,15 +1,18 @@
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
 import { Kxkagencyheader } from '../kxkagencyheader/kxkagencyheader';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-projects',
-  imports: [Kxkagencyheader],
+  imports: [Kxkagencyheader, ButtonModule],
   templateUrl: './projects.html',
   styleUrl: './projects.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Projects implements OnInit, OnDestroy{
   public colors = ['#555555', '#989898', '#dbdbdb', '#323232', '#808080', '#dadada', '#ffffff', '#a5a5a5'];
+
+  public filters = ['всё', 'amazon', 'айдентика', 'упаковка', 'иллюстрации', 'штрифт', 'ретушь', 'ии-аватары', 'дизайн музыки', 'книги']
 
   constructor(private changeDetection: ChangeDetectorRef,){
     
